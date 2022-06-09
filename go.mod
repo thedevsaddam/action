2393dd25c9b3e3ea7a1a0a42e7851969c87e3255
marcelosousa <601882+marcelosousa@users.noreply.github.com>
2022-06-09 08:57:29 +0100

    Add bundle code

---
+++ go.mod
@@ -0,0 +1,30 @@
+module github.com/reviewpad/action
+
+go 1.18
+
+require (
+	github.com/explore-dev/atlas-common/go v0.0.0-20220509090605-a2bc1701df68
+	github.com/google/go-github/v42 v42.0.0
+	github.com/reviewpad/reviewpad v0.0.0-20220608111215-f23e7b3d5547
+	github.com/reviewpad/reviewpad-premium v0.0.0-20220608111911-7f241d819af7
+	github.com/shurcooL/githubv4 v0.0.0-20220520033151-0b4e3294ff00
+	golang.org/x/oauth2 v0.0.0-20220524215830-622c5d57e401
+	google.golang.org/grpc v1.40.0
+)
+
+require (
+	github.com/bmatcuk/doublestar/v4 v4.0.2 // indirect
+	github.com/dukex/mixpanel v0.0.0-20220410140740-e82251311162 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/shurcooL/graphql v0.0.0-20220520033453-bdb1221e171e // indirect
+	github.com/tomnomnom/linkheader v0.0.0-20180905144013-02ca5825eb80 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,153 @@
+// Copyright 2022 Explore.dev Unipessoal Lda. All Rights Reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	atlas "github.com/explore-dev/atlas-common/go/api/services"
+	"github.com/google/go-github/v42/github"
+	"github.com/reviewpad/reviewpad"
+	reviewpad_premium "github.com/reviewpad/reviewpad-premium"
+	"github.com/reviewpad/reviewpad/collector"
+	"github.com/reviewpad/reviewpad/engine"
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
+	"google.golang.org/grpc"
+)
+
+var MixpanelToken string
+
+const ReviewpadFile string = "reviewpad.yml"
+
+type Env struct {
+	RepoOwner        string
+	RepoName         string
+	Token            string
+	PRNumber         int
+	SemanticEndpoint string
+}
+
+func getEnv() (*Env, error) {
+	repo, ok := os.LookupEnv("INPUT_REPOSITORY")
+	if !ok {
+		return nil, fmt.Errorf("missing input repository")
+	}
+
+	repoStrings := strings.Split(repo, "/")
+	if len(repoStrings) != 2 {
+		return nil, fmt.Errorf("error parsing repository owner and name")
+	}
+	repoOwner := repoStrings[0]
+	repoName := repoStrings[1]
+
+	token, ok := os.LookupEnv("INPUT_TOKEN")
+	if !ok {
+		return nil, fmt.Errorf("missing token")
+	}
+
+	prnumStr, ok := os.LookupEnv("INPUT_PRNUMBER")
+	if !ok {
+		return nil, fmt.Errorf("missing pull request number")
+	}
+
+	prNum, err := strconv.Atoi(prnumStr)
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving pull request number %v", err)
+	}
+
+	semanticEndpoint, ok := os.LookupEnv("SEMANTIC_SERVICE_ENDPOINT")
+	if !ok {
+		return nil, fmt.Errorf("missing semantic service endpoint")
+	}
+
+	return &Env{
+		RepoOwner:        repoOwner,
+		RepoName:         repoName,
+		Token:            token,
+		PRNumber:         prNum,
+		SemanticEndpoint: semanticEndpoint,
+	}, nil
+}
+
+func main() {
+	env, err := getEnv()
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: env.Token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+	clientGQL := githubv4.NewClient(tc)
+
+	ghPullRequest, _, err := client.PullRequests.Get(ctx, env.RepoOwner, env.RepoName, env.PRNumber)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	if ghPullRequest.Merged != nil && *ghPullRequest.Merged {
+		log.Print("skip execution for merged pull requests")
+		return
+	}
+
+	// TODO: Extend logic to choose between base or head
+	// TODO: Check for nils
+	headRepoOwner := *ghPullRequest.Head.Repo.Owner.Login
+	headRepoName := *ghPullRequest.Head.Repo.Name
+	headRef := *ghPullRequest.Head.Ref
+
+	ioReader, _, err := client.Repositories.DownloadContents(ctx, headRepoOwner, headRepoName, ReviewpadFile, &github.RepositoryContentGetOptions{
+		Ref: headRef,
+	})
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(ioReader)
+
+	file, err := reviewpad.Load(buf)
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+
+	collectorClient := collector.NewCollector(MixpanelToken, headRepoOwner)
+
+	defaultOptions := grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(419430400))
+	semanticConnection, err := grpc.Dial(env.SemanticEndpoint, grpc.WithInsecure(), defaultOptions)
+	if err != nil {
+		log.Printf("failed to dial semantic service: %v", err)
+		return
+	}
+	defer semanticConnection.Close()
+	semanticClient := atlas.NewSemanticClient(semanticConnection)
+
+	switch file.Edition {
+	case engine.PROFESSIONAL_EDITION:
+		err = reviewpad_premium.Run(ctx, client, clientGQL, collectorClient, semanticClient, ghPullRequest, file, false)
+	default:
+		err = reviewpad.Run(ctx, client, clientGQL, collectorClient, ghPullRequest, file, false)
+	}
+
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+}
